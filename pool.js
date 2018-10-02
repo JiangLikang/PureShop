6c23ee679470c35780a036abaa6d9d7e559b86e2
jlk <1848399033@qq.com>
2018-10-02 21:25:36 +0800

    'describe'

--- pool.js
+++ pool.js
@@ -2,8 +2,8 @@ const mysql = require('mysql');
 //连接mysql数据库的模块
 var pool = mysql.createPool({
 	host: 'w.rdc.sae.sina.com.cn',
-	user: 'root',
-	passward: '123456',
+	user: 'mjz1ylm53w',
+	passward: 'j3k5zx20wmx42l4iw30w023zmj4jyk0mzll40lw0',
 	database: 'app_pureshop',
 });
 //导出连接数据库
