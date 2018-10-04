b3b089693c14d1fd013335581ef6f44d9821faa0
jlk <1848399033@qq.com>
2018-10-04 13:17:57 +0800

    'des'

--- app.js
+++ app.js
@@ -10,11 +10,12 @@ const index = require('./routers/index');
 const products_list = require('./routers/products_list');
 
 var app = express();
-app.use(function(req, res, next) {
+app.all('*', function(req, res, next) {
+	console.log(req.method);
 	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-	res.header("Access-Control-Allow-Headers", "Content-Type");
-	res.header("Access-Control-Allow-Credentials", "true");
+	res.header('Access-Control-Allow-Headers', 'Content-type');
+	res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS,PATCH");
+	res.header('Access-Control-Max-Age', 1728000); //预请求缓存20天
 	next();
 });
 app.listen(5050);
