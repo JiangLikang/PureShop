877a3d74ac40d67e2fa7b9ab73ca9090fe000192
jlk <1848399033@qq.com>
2018-10-05 16:03:49 +0800

    'd'

--- pool.js
+++ pool.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql');
 //连接mysql数据库的模块
 var pool = mysql.createPool({
-	host: 'localhost',
+	host: '	w.rdc.sae.sina.com.cn',
 	port: 3307,
 	user: 'mjz1ylm53w',
 	passward: 'j3k5zx20wmx42l4iw30w023zmj4jyk0mzll40lw0',
