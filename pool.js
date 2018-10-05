70cad71146edf5bac24abc8ce634dd772912d294
jlk <1848399033@qq.com>
2018-10-05 15:43:38 +0800

    'd'

--- pool.js
+++ pool.js
@@ -1,8 +1,8 @@
 const mysql = require('mysql');
 //连接mysql数据库的模块
 var pool = mysql.createPool({
-	host: 'pureshop.applinzi.com',
-	port: 3306,
+	host: 'w.rdc.sae.sina.com.cn',
+	port: 3307,
 	user: 'mjz1ylm53w',
 	passward: 'j3k5zx20wmx42l4iw30w023zmj4jyk0mzll40lw0',
 	database: 'app_pureshop',
