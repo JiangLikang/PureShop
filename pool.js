dbd14f23fd06484704c8d5d7bc5bc8333704bc1d
jlk <1848399033@qq.com>
2018-10-09 21:19:07 +0800

    'd'

--- pool.js
+++ pool.js
@@ -4,7 +4,7 @@ var pool = mysql.createPool({
 	host: 'w.rdc.sae.sina.com.cn',
 	port: 3307,
 	user: 'mjz1ylm53w',
-	passward: 'j3k5zx20wmx42l4iw30w023zmj4jyk0mzll40lw0',
+	password: 'j3k5zx20wmx42l4iw30w023zmj4jyk0mzll40lw0',
 	database: 'app_pureshop'
 });
 //导出连接数据库
