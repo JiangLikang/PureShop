d57419940ce3d416d63b9e81bbf25bd62faa85c0
jlk <1848399033@qq.com>
2018-10-09 19:11:29 +0800

    'd'

--- pool.js
+++ pool.js
@@ -1,11 +1,18 @@
 const mysql = require('mysql');
 //连接mysql数据库的模块
-var pool = mysql.createPool({
+// var pool = mysql.createPool({
+// 	host: '	w.rdc.sae.sina.com.cn',
+// 	port: 3307,
+// 	user: 'mjz1ylm53w',
+// 	passward: 'j3k5zx20wmx42l4iw30w023zmj4jyk0mzll40lw0',
+// 	database: 'app_pureshop',
+// });
+var connection = mysql.createConnection({
 	host: '	w.rdc.sae.sina.com.cn',
 	port: 3307,
 	user: 'mjz1ylm53w',
 	passward: 'j3k5zx20wmx42l4iw30w023zmj4jyk0mzll40lw0',
-	database: 'app_pureshop',
+	database: 'app_pureshop'
 });
 //导出连接数据库
-module.exports = pool;
+module.exports = connection;
