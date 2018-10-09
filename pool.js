0de48b9c73542e29bbc0863a1348a80bde4c8300
jlk <1848399033@qq.com>
2018-10-09 20:40:51 +0800

    'd'

--- pool.js
+++ pool.js
@@ -7,12 +7,5 @@ var pool = mysql.createPool({
 	passward: 'j3k5zx20wmx42l4iw30w023zmj4jyk0mzll40lw0',
 	database: 'app_pureshop'
 });
-// var connection = mysql.createConnection({
-// 	host: '	w.rdc.sae.sina.com.cn',
-// 	port: 3307,
-// 	user: 'mjz1ylm53w',
-// 	passward: 'j3k5zx20wmx42l4iw30w023zmj4jyk0mzll40lw0',
-// 	database: 'app_pureshop'
-// });
 //导出连接数据库
 module.exports = pool;
