1e2d8162057fd68f6bce6091d83268ce4495345a
jlk <1848399033@qq.com>
2018-10-10 10:10:49 +0800

    'd'

--- static/js/user_login.js
+++ static/js/user_login.js
@@ -16,37 +16,34 @@
 					uname: this.uname,
 					upwd: this.upwd
 				}));
-				if (res.data.ok == 1) {
-					// alert(res.data.msg);
-					// location.href = 'http://localhost:8080/index.html';
-					swal({
-						title: "",
-						text: "登录成功！",
-						icon: "success",
-						button: {
-							text: "确定",
-							value: true
-						}
-					}).then(function(value) {
-						if (value) {
+				// if (res.data.ok == 1) {
+				// 	swal({
+				// 		title: "",
+				// 		text: "登录成功！",
+				// 		icon: "success",
+				// 		button: {
+				// 			text: "确定",
+				// 			value: true
+				// 		}
+				// 	}).then(function(value) {
+				// 		if (value) {
 
-							self.location = document.referrer;
-						}
-					})
+				// 			self.location = document.referrer;
+				// 		}
+				// 	})
 
-				} else {
-					// alert(res.data.msg);
-					swal({
-						title: "",
-						text: res.data.msg,
-						icon: "error",
-						button: {
-							text: "确定",
-							value: true
-						}
-					})
-				}
-
+				// } else {
+				// 	swal({
+				// 		title: "",
+				// 		text: res.data.msg,
+				// 		icon: "error",
+				// 		button: {
+				// 			text: "确定",
+				// 			value: true
+				// 		}
+				// 	})
+				// }
+				console.log(res.data);
 			}
 		}
 
