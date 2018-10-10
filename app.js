2fe6ff6632d1d539e6535baeca3eb7d29553b337
jlk <1848399033@qq.com>
2018-10-10 10:40:54 +0800

    'd'

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const products_list = require('./routers/products_list');
 
 var app = express();
 app.all('*', function(req, res, next) {
-	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Origin", "http://pureshop.applinzi.com");
 	res.header('Access-Control-Allow-Headers', 'Content-type');
 	res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS,PATCH");
 	res.header('Access-Control-Max-Age', 1728000); //预请求缓存20天
