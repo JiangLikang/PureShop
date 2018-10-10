1718339cf87cad7e044dc01061baeb9358e2f421
jlk <1848399033@qq.com>
2018-10-10 11:38:45 +0800

    'd'

--- static/js/user_login.js
+++ static/js/user_login.js
@@ -17,34 +17,34 @@
 					uname: this.uname,
 					upwd: this.upwd
 				}));
-				// if (res.data.ok == 1) {
-				// 	swal({
-				// 		title: "",
-				// 		text: "登录成功！",
-				// 		icon: "success",
-				// 		button: {
-				// 			text: "确定",
-				// 			value: true
-				// 		}
-				// 	}).then(function(value) {
-				// 		if (value) {
+				if (res.data.ok == 1) {
+					swal({
+						title: "",
+						text: "登录成功！",
+						icon: "success",
+						button: {
+							text: "确定",
+							value: true
+						}
+					}).then(function(value) {
+						if (value) {
 
-				// 			self.location = document.referrer;
-				// 		}
-				// 	})
+							self.location = document.referrer;
+						}
+					})
 
-				// } else {
-				// 	swal({
-				// 		title: "",
-				// 		text: res.data.msg,
-				// 		icon: "error",
-				// 		button: {
-				// 			text: "确定",
-				// 			value: true
-				// 		}
-				// 	})
-				// }
-				console.log(res.data);
+				} else {
+					swal({
+						title: "",
+						text: res.data.msg,
+						icon: "error",
+						button: {
+							text: "确定",
+							value: true
+						}
+					})
+				}
+				// console.log(res.data);
 			}
 		}
 
